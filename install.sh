e5239ebb9dfac8f1f490980a58548f49286a9575
Julian de Almeida Santos <julian.santos.info@gmail.com>
2026-03-08 14:10:57 -0300

    feat: License Server version to 3.6.1 across Dockerfile, README, and environment variables.

--- install.sh
+++ install.sh
@@ -6,31 +6,60 @@ set -euo pipefail
 TOTVS_DIR="/totvs"
 INSTALL_DIR="/totvs/licenseserver"
 
+rm -rf "$INSTALL_DIR"
+
 cd "$TOTVS_DIR"
 
 echo "🚀 Iniciando instalação automática..."
 
 # Envia respostas automáticas para o instalador
 {
-    echo "0"              # Select your language
-    echo "1"              # Confirma
-    echo "1"              # Acordo de licença
-    echo "$INSTALL_DIR"   # Diretório de instalação
-    echo "S"              # Enter para continuar
-    echo "1"              # Confirma 
-    echo "0"              # Instalação / Atualização
-    echo "1"              # Confirma 
-    echo "S"              # Include optional pack 'TOTVS | License Server Virtual'
-    echo "1"              # Confirma 
-    echo "1"              # Informações
-    echo "2234"           # Porta de Monitoramento/Manutenção
-    echo "4000"           # Porta do Serviço do Servidor de Log
-    echo "5555"           # Porta do Serviço de Licenciamento. (conexão do ERP)
-    echo "licenseVirtual" # Nome do serviço de Monitoramento/Manutenção
-    echo "1"              # Confirma 
-    echo ""               # Habilitar Proxy?
-    echo "1"              # Confirma 
-    echo "S"              # Confirma 
+    echo ""               # TERMOS E CONDIÇÕES 1/3
+    sleep 2
+    echo ""               # TERMOS E CONDIÇÕES 2/3
+    sleep 2
+    echo ""               # TERMOS E CONDIÇÕES 3/3
+    sleep 2
+    echo "1"              # TERMOS E CONDIÇÕES | press 1 to accept, 2 to reject, 3 to redisplay
+    sleep 2
+
+    echo "$INSTALL_DIR"   # Diretório de instalação /totvs/licenseserver
+    sleep 2
+
+    echo "1"              # Enter para continuar
+    sleep 2
+
+    echo ""               # Enter para continuar
+    sleep 2
+
+    echo "1"              # Enter para continuar
+    sleep 2
+
+    echo ""               # Enter para continuar | TOTVS | License Server Virtual (Instalação do TOTVS | License Server Virtual.)
+    sleep 2
+    echo ""               # Enter para continuar | TOTVS | License Server Virtual - RPO (Instalação do TOTVS | License Server Virtual - RPO.)
+    sleep 2
+    echo ""               # Enter para continuar | TOTVS | License Server Virtual - Binarios (Instalação do TOTVS | License Server Virtual - Binarios.)
+    sleep 2
+
+    echo "1"              # Enter para continuar | press 1 to continue, 2 to quit, 3 to redisplay
+    sleep 2
+
+    echo ""               # Enter para continuar | null [2234]
+    sleep 2
+
+    echo ""               # Enter para continuar | null [4000]
+    sleep 2
+
+    echo ""               # Enter para continuar | null [5555]
+    sleep 2
+
+    echo ""               # Enter para continuar | null [licenseVirtual]
+    sleep 2
+
+    echo "1"              # Enter para continuar | press 1 to continue, 2 to quit, 3 to redisplay
+    sleep 2
+
 } | java -jar installer.jar
 
 # Validação de retorno
